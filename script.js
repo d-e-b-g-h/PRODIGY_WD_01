886d76616b5be335735d0ce1147d661151935bf1
d-e-b-g-h <166236904+d-e-b-g-h@users.noreply.github.com>
2025-10-03 21:09:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const navbar = document.getElementById("navbar");
 
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("scrolled");
+  } else {
+    navbar.classList.remove("scrolled");
+  }
+});
